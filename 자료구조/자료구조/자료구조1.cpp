fef32eaf451d480581bb4a1753cab86ab3c02c44
EmotionalRealizer <tkdqor3553@naver.com>
2023-08-03 10:19:18 +0900

    큐구현해보기

--- 자료구조/자료구조/자료구조1.cpp
+++ 자료구조/자료구조/자료구조1.cpp
@@ -3,7 +3,7 @@
 
 
 //스택 코드 구현
-
+/*
 typedef struct stacktype {
 	int arr[100];
 	int top;
@@ -88,3 +88,70 @@ int main()
 
 	return 0;
 }
+*/
+
+
+//큐 구현 
+
+#define MAX 5
+
+typedef struct Queue {
+	int front; //앞 쪽 index
+	int rear;  //뒤 쪽 index
+	int data[MAX];
+} Queue;
+
+void init(Queue* q)
+{
+	q->front = q->rear = -1;
+}
+
+int is_full(Queue *q){
+	if (q->rear == MAX - 1)
+		return 1;
+	return 0;
+}
+
+int is_empty(Queue *q){
+	if (q->front == q->rear)
+		return 1;
+	return 0;
+}
+
+void enqueue(Queue* q, int item)
+{
+	if(is_full(q))
+	{
+		printf("Error Queue is full");
+		exit(1);
+	}
+	q->data[++(q->rear)] = item;
+}
+
+int dequeue(Queue* q) {
+	if (is_empty(q))
+	{
+		printf("Error Queue is empty");
+		exit(1);
+	}
+	return q->data[++(q->front)];      ///////////
+}
+
+int main()
+{
+	Queue q;
+	init(&q);
+
+	enqueue(&q, 3);
+	enqueue(&q, 2);
+	enqueue(&q, 1);
+
+	printf("%d\n", dequeue(&q));
+	printf("%d\n", dequeue(&q));
+	printf("%d\n", dequeue(&q));
+
+
+	return 0;
+}
+
+//원형큐 
